39f896a2d4c4c7147db9a48f6d16b4a502205e0f
nickbullington <56139459+nickbullington@users.noreply.github.com>
2023-11-26 21:02:23 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,22 +4,27 @@ import numpy as np
 import plotly.express as px
 
 st.title('WASDE Corn Analysis')
-string = r'C:\Users\nb153794\OneDrive - The Andersons, Inc\Desktop' \
-    r'\Work Projects\wasde analysis\grain Corn.csv'
 
 @st.cache_data
-def data_pull(string_path):
-    df = pd.read_csv(string_path)
-    df['release_date'] = pd.to_datetime(df['release_date'])
-    df = df.set_index('release_date')
-    return df
+def pull_inspection_data(string, year_lst):
+    df_lst = []
+    for i in year_lst:
+        file_path = string + '/CY' + i + '.csv'
+        df = pd.read_csv(file_path, usecols=["Thursday", "Grain", "Class", "Destination", "Metric Ton"])
+        df_lst.append(df)
+    out_df = pd.concat(df_lst, axis=0)
+    return out_df
+
+string = 'https://fgisonline.ams.usda.gov/ExportGrainReport'
+year_lst = ['2019', '2020', '2021', '2022', '2023']
+raw_df = pull_inspection_data(string, year_lst)
 
 data = data_pull(string)
 converted_data = data.to_csv().encode('utf-8')
 st.download_button('Historical WASDE Corn Data', data=converted_data, file_name='wasde_df.csv', mime='text/csv')
-st.plotly_chart(px.line(data))
-st.title('Ending Stocks')
+#st.plotly_chart(px.line(data))
+#st.title('Ending Stocks')
 
-st.plotly_chart(px.histogram(data['ending_stocks_total']))
+#st.plotly_chart(px.histogram(data['ending_stocks_total']))
 #st.dataframe(data)
-st.text('Hey, Kerianne. What is your favorite food?')
+#st.text('Hey, Kerianne. What is your favorite food?')
