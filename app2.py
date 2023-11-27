933a5e2db95fcf9315dd8c2aa0e26f1011528cc5
nickbullington <56139459+nickbullington@users.noreply.github.com>
2023-11-26 22:17:43 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -6,7 +6,7 @@ import plotly.graph_objects as go
 
 st.title('Weekly Export Inspections')
 
-@st.cache_data
+@st.cache_data(ttl=20*60)
 def pull_inspection_data(string, year_lst):
     df_lst = []
     for i in year_lst:
