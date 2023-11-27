bc325b0b0cdbdb05f97ff26b3877c68b720b423b
nickbullington <56139459+nickbullington@users.noreply.github.com>
2023-11-26 22:29:32 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -150,7 +150,7 @@ def build_charts(inspections_dct, estimated_exports_dct, dest_sum_df):
                    font_size=13,
                    align='center',
                    height=20))])
-    dest_fig.update_layout(title='Export Inspections Summary (metric tons)', height=len(dest_sum_df.T) * 35, width=700)
+    dest_fig.update_layout(title='Export Inspections Summary (metric tons)', height=len(dest_sum_df) * 35, width=700)
     
     fig_dct['dest_sum_table'] = dest_fig
     
