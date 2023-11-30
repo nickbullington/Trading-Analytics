92568547b7e2951ee30cd11c62b0b5a2974d60bd
nickbullington <56139459+nickbullington@users.noreply.github.com>
2023-11-30 07:55:06 -0600

    Delete pages/2Future Tab Ideas.py

--- pages/2Future Tab Ideas.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-st.subheader('Ideas for future tabs')
-st.text('Anamoly Finder. Find and display properly adjusted (trend, seasonality, fundamental factors) relationships that are far from normal.')
-st.text('Fertilizer hub')
-st.text('model builder')
-st.text('price predictions')
