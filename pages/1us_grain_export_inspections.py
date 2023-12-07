7733a6a7a9d124b26f093c525474d35ea28e7420
nickbullington <56139459+nickbullington@users.noreply.github.com>
2023-12-07 14:22:49 -0600

    Update 1us_grain_export_inspections.py

--- pages/1us_grain_export_inspections.py
+++ pages/1us_grain_export_inspections.py
@@ -220,10 +220,10 @@ def build_charts(inspections_dct, estimated_exports_dct, dest_sum_df):
 est_exports_dct = {'SOYBEANS': 1685000000 / 36.73,
              'CORN': 1800000000 / 39.35,
              'SORGHUM': 215000000 / 39.35,
-             'WHEAT-HRS': 206000000 / 36.73,
-             'WHEAT-HRW': 133000000 / 36.73,
-             'WHEAT-SRW': 147000000 / 36.73,
-             'WHEAT-SWW': 134000000 / 36.73,
+             'WHEAT-HRS': 220000000 / 36.73,
+             'WHEAT-HRW': 145000000 / 36.73,
+             'WHEAT-SRW': 180000000 / 36.73,
+             'WHEAT-SWW': 155000000 / 36.73,
              'WHEAT-ALL': 686000000 / 36.73} #'WHEAT-DUWH': 2000000}
 
 fig_dct = build_charts(annual_inspections_dct, est_exports_dct, dest_sum_df)
